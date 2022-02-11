15e061d79de0b07e65bd2e64d1bec6d7fc814c35
Karim Khasan <karimhasan3879@gmail.com>
2022-02-11 23:25:46 +0300

    Added solution for task1.6

--- Lab1/task6.cpp
+++ Lab1/task6.cpp
@@ -1,7 +1,5 @@
 #include <fstream>
 #include <vector>
-#include <iostream>
-#include <set>
 #include <string>
 #include <queue>
 using namespace std;
@@ -13,74 +11,40 @@ struct Vertex {
     bool checked = false;
 };
 
-void bfs(vector<vector<int>> const &adj_list, vector<Vertex> &vertexes, \
-        int start_point, int exit_point);
+void bfs(vector<vector<char>> const &labyrinth, vector<Vertex> &vertexes, \
+        int const &start_point, int const &exit_point);
+vector<int> find_connections(vector<vector<char>> const &labyrinth, int const &check,\
+                            int const &exit_point);
+string find_path(vector<Vertex> const &vertexes, int const &exit_point);
+int n, m;
 
 
 int main() {
     ifstream fin("input.txt");
     ofstream fout("output.txt");
     
-    int n, m;
     fin >> n >> m;
-    vector<vector<char>> labyrinth(n + 2, vector<char>(m + 2));
+    vector<vector<char>> labyrinth(n, vector<char>(m));
     int starting_point, exit_point;
-    for (size_t i = 1; i < n + 1; ++i) {
-        for (size_t j = 1; j < m + 1; ++j) {
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < m; ++j) {
             char labyrinth_element;
             fin >> labyrinth_element;
             if (labyrinth_element == 'S') {
-                starting_point = (i - 1) * m + (j - 1);
+                starting_point = i * m + j;
             }
             else if (labyrinth_element == 'T') {
-                exit_point = (i - 1) * m + (j - 1);
+                exit_point = i * m + j;
             }
             labyrinth[i][j] = labyrinth_element;
         }
     }
 
-    vector<vector<int>> adj_list(n * m, vector<int>());
-    for (size_t i = 1; i < n + 1; ++i) {
-        for (size_t j = 1; j < m + 1; ++j) {
-            int vertex_index = (i - 1) * m + (j - 1);
-            if (labyrinth[i - 1][j] == '.' || labyrinth[i - 1][j] == 'T') {
-                adj_list[vertex_index].push_back((i - 2) * m + (j - 1));
-            }
-            if (labyrinth[i][j - 1] == '.' || labyrinth[i][j - 1] == 'T') {
-                adj_list[vertex_index].push_back((i - 1) * m + (j - 2));
-            }
-            if (labyrinth[i + 1][j] == '.' || labyrinth[i + 1][j] == 'T') {
-                adj_list[vertex_index].push_back(i * m + (j - 1));
-            }
-            if (labyrinth[i][j + 1] == '.' || labyrinth[i][j + 1] == 'T') {
-                adj_list[vertex_index].push_back((i - 1) * m + j);
-            }
-        }
-    }
-    
     vector<Vertex> vertexes(n * m, Vertex());
-    bfs(adj_list, vertexes, starting_point, exit_point);
+    bfs(labyrinth, vertexes, starting_point, exit_point);
     
     if (vertexes[exit_point].checked) {
-        int curr_index = exit_point;
-        int parent_index = vertexes[exit_point].parent;
-        string path;
-        while (parent_index != -1) {
-            if (parent_index == curr_index - 1) {
-                path = "R" + path;
-            } 
-            else if (parent_index == curr_index - m) {
-                path = "D" + path;
-            }
-            else if (parent_index == curr_index + 1) { 
-                path = "L" + path;
-            }
-            else if (parent_index == curr_index + m) {
-                path = "U" + path;
-            }
-            curr_index = parent_index;
-            parent_index = vertexes[parent_index].parent;
-        }
+        string path = find_path(vertexes, exit_point);
         fout << vertexes[exit_point].distance << '\n' << path;
     }
     else {
@@ -91,33 +55,78 @@ int main() {
 }
 
 
-void bfs(vector<vector<int>> const &adj_list, vector<Vertex> &vertexes,\
-         int start_point, int exit_point) {
+void bfs(vector<vector<char>> const &labyrinth, vector<Vertex> &vertexes,\
+         int const &start_point, int const &exit_point) {
 
     vertexes[start_point].checked = true;
     queue<int> vertexes_queue;
     vertexes_queue.push(start_point);
-
     while (vertexes_queue.size() != 0) {
 
-        int checked_index = vertexes_queue.front();
+        int check = vertexes_queue.front();
         vertexes_queue.pop();
+        vertexes[check].checked = true;
+        vector<int> adj_list = find_connections(labyrinth, check, exit_point);
 
-        for (size_t i = 0; i < adj_list[checked_index].size(); ++i) {
-
-            int adj_position = adj_list[checked_index][i];
-            if (vertexes[adj_position].checked == false) {
-                vertexes[adj_position].checked = true;
-                vertexes[adj_position].distance = vertexes[checked_index].distance + 1;
-                vertexes[adj_position].parent = checked_index;
-                vertexes_queue.push(adj_position);
+        for (vector<int>::iterator i = adj_list.begin(); i != adj_list.end(); ++i) {
+            if (vertexes[*i].checked == false) {
+                vertexes[*i].checked = true;
+                vertexes[*i].parent = check;
+                vertexes[*i].distance = vertexes[check].distance + 1;
+                vertexes_queue.push(*i);
             }
-
-            if (adj_position == exit_point) {
+            if (*i == exit_point) {
                 return;
             }
-
         }
 
     }
+}
+
+
+vector<int> find_connections(vector<vector<char>> const &labyrinth, int const &check,\
+                            int const &exit_point) {
+    
+    vector<int> adj_list;
+    int y_pos = check / m;
+    int x_pos = check % m;
+
+    if (x_pos > 0 && (labyrinth[y_pos][x_pos - 1] == '.' || check - 1 == exit_point)) {
+        adj_list.push_back(check - 1);
+    }
+    if (x_pos < m - 1 && (labyrinth[y_pos][x_pos + 1] == '.' || check + 1 == exit_point)) {
+        adj_list.push_back(check + 1);
+    }
+    if (y_pos > 0 && (labyrinth[y_pos - 1][x_pos] == '.' || check - m == exit_point)) {
+        adj_list.push_back(check - m);
+    }
+    if (y_pos < n - 1 && (labyrinth[y_pos + 1][x_pos] == '.' || check + m == exit_point)) {
+        adj_list.push_back(check + m);
+    }
+
+    return adj_list;
+}
+
+
+string find_path(vector<Vertex> const &vertexes, int const &exit_point) {
+    string path;
+    int curr_index = exit_point;
+    int parent_index = vertexes[exit_point].parent;
+    while (parent_index != -1) {
+        if (parent_index == curr_index - 1 && parent_index / m == curr_index / m) {
+            path = "R" + path;
+        }
+        else if (parent_index == curr_index - m) {
+            path = "D" + path;
+        }
+        else if (parent_index == curr_index + 1 && parent_index / m == curr_index / m) {
+            path = "L" + path;
+        }
+        else if (parent_index == curr_index + m) {
+            path = "U" + path;
+        }
+        curr_index = parent_index;
+        parent_index = vertexes[parent_index].parent;
+    }
+    return path;
 }
