4d5b8af6ebcb5fdad5faadab7268b6ccd2ea73f0
Karim Khasan <karimhasan3879@gmail.com>
2022-04-22 21:19:58 +0300

    Added solutions for task5.1 and task5.2

---
+++ Lab5/task1_n2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+using namespace std;
+
+vector<int> find_lis(vector<int> const& data);
+
+size_t n;
+int main() {
+    cin >> n;
+    
+    vector<int> data(n);
+    for (size_t i = 0; i < n; ++i) {
+        cin >> data[i];
+    }
+
+    vector<int> lis = find_lis(data);
+    cout << lis.size() << "\n";
+    for (auto value: lis) {
+        cout << value << " ";
+    }
+
+    return 0;
+}
+
+
+vector<int> find_lis(vector<int> const& data) {
+    vector<int> lis_lenghts(n, 1);
+    vector<int> prev(n, -1);
+
+    for (size_t i = 1; i < n; ++i) {
+        for (size_t j = 0; j < i; ++j) {
+            if (data[i] > data[j] && lis_lenghts[j] + 1 > lis_lenghts[i]) {
+                lis_lenghts[i] = lis_lenghts[j] + 1;
+                prev[i] = j;
+            }
+        }
+    }
+
+    size_t max_length_index = max_element(lis_lenghts.begin(), lis_lenghts.end()) - lis_lenghts.begin();
+    size_t lis_size = lis_lenghts[max_length_index];  
+    vector<int> ans(lis_size);
+    for (int curr_element = max_length_index, i = lis_size - 1; curr_element != -1; \
+    curr_element = prev[curr_element], --i) {
+        ans[i] = data[curr_element];
+    }
+    return ans;
+}
---
+++ Lab5/task1_nlogn.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+using namespace std;
+
+constexpr int INF = numeric_limits<int>::max();
+
+size_t binsearch(vector<int>& lis, int value, size_t curr_len);
+vector<int> find_lis(vector<int> const& data, size_t& curr_len);
+
+size_t n;
+int main() {
+ 
+    cin >> n;
+    vector<int> data(n);
+    size_t curr_len = 0;
+
+    for (size_t i = 0; i < n; ++i) {
+        cin >> data[i];
+    }
+
+    vector<int> lis = find_lis(data, curr_len);
+    cout << curr_len << "\n";
+
+    for (auto value : lis) {
+        cout << value << " ";
+    }
+    return 0;
+}
+
+vector<int> find_lis(vector<int> const& data, size_t& curr_len) {
+
+    vector<int> lis_min_elements(n, INF);
+    vector<int> prev(n, -1);
+    vector<int> indexes(n, -1);
+
+    for (size_t i = 0; i < n; ++i) {
+        size_t position = binsearch(lis_min_elements, data[i], curr_len);
+        lis_min_elements[position] = data[i];
+        indexes[position] = i;
+
+        if (position > 0) {
+            prev[i] = indexes[position - 1];
+        }
+        if (position >= curr_len) {
+            ++curr_len;
+        }
+    }
+    
+    vector<int> ans(curr_len);
+    for (int curr_index = indexes[curr_len - 1], i = curr_len - 1; curr_index != -1; \
+    curr_index = prev[curr_index], --i) {
+        ans[i] = data[curr_index];
+    }
+    return ans;
+}
+
+
+size_t binsearch(vector<int>& lis, int value, size_t curr_len) {
+    int left = 0, right = curr_len;
+    size_t position = 0;
+    while (left < right) {
+        position = (left + right) / 2;
+        if (lis[position] == value) {
+            return position;
+        }
+        else if (lis[position] > value) {
+            right = position;
+        }
+        else {
+            left = position + 1;
+        }
+    }
+    if (lis[position] > value) {
+        return position;
+    }
+    return position + 1;
+}
