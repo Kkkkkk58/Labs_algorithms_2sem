a1a49bc3c1a5a8335214f612dfff7ac65675a0f0
Karim Khasan <karimhasan3879@gmail.com>
2022-05-06 22:43:10 +0300

    Added solution for task6.3

---
+++ Lab6/task3.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void build_prefix(string const& text, vector<size_t> &prefix);
+
+int main() {
+    ifstream fin("prefix.in");
+    ofstream fout("prefix.out");
+    string text;
+    fin >> text;
+
+    vector<size_t> prefix(text.size() + 1);
+    build_prefix(text, prefix);
+
+    for (size_t i = 1; i < prefix.size(); ++i) {
+        fout << prefix[i] << " ";
+    }
+    return 0;
+}
+
+
+void build_prefix(string const& text, vector<size_t> &prefix) {
+    if (text.size() == 1) {
+        return;
+    }
+
+    size_t i = 1, j = 0;
+    while (i <= text.size()) {
+        if (text[i] == text[j]) {
+            prefix[i + 1] = j + 1;
+            ++i;
+            ++j;
+        }
+
+        else if (j != 0) {
+            j = prefix[j];
+        }
+        
+        else {
+            prefix[i + 1] = 0;
+            ++i;
+        }
+    }
+}
